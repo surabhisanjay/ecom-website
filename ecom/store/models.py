6f90cf523003af2bbfa63fcdf3d01b398d7f01cf
surabhisanjay <surabhisanjay8@gmail.com>
2026-04-11 19:13:36 +0530

    merged migrations, change button colours, removed redundant home page

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -33,13 +33,7 @@ class Product(models.Model):
 # =========================
 # CART MODEL (NEW)
 # =========================
-class CartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
 
-    def __str__(self):
-        return f"{self.user.username} - {self.product.name}"
 
 
 # =========================
